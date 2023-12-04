9f5a67d0928e0930ef12386dd020acba9bb0e785
Rob2U <robert.weeke@student.hpi.uni-potsdam.de>
2023-12-04 17:26:44 +0000

    improve readability of tripletloss offline test

--- tests/test_tripletloss.py
+++ tests/test_tripletloss.py
@@ -8,6 +8,8 @@ def calc_loss_of_triplet(anchor, positive, negative, margin):
     loss = torch.relu(
         torch.linalg.vector_norm(anchor - positive) - torch.linalg.vector_norm(anchor - negative) + margin
     )
+    assert approx_equal(loss, TripletMarginLoss(margin=margin)(anchor, positive, negative))
+
     return loss
 
 
@@ -25,22 +27,28 @@ def approx_equal(a, b, eps=1e-6):
 def test_tripletloss_offline():
     a, p, n = torch.tensor([1.0]), torch.tensor([0.5]), torch.tensor([-1.0])
     labels = torch.tensor([0, 0, 1])
+
     triplet_loss = TripletLossOffline(margin=1.0)
     sample_triplet = torch.stack([a, p, n])
+
     loss, d_p, d_n = triplet_loss(sample_triplet, labels)
-    loss_manual = calc_loss_of_triplet(a, p, n, margin=1.0)
-    assert loss == loss_manual and loss == 0.0
+    loss_correct = calc_loss_of_triplet(a, p, n, margin=1.0)
+
+    assert loss == loss_correct and loss == 0.0
     assert d_p == torch.linalg.norm(a - p)
     assert d_n == torch.linalg.norm(a - n)
 
     a, p, n = torch.tensor([1.0]), torch.tensor([0.5]), torch.tensor([0.5])
     loss, d_p, d_n = triplet_loss(torch.stack([a, p, n]), labels)
-    assert approx_equal(loss, calc_loss_of_triplet(a, p, n, margin=1.0)) and approx_equal(loss, 1.0)
+
+    assert approx_equal(loss, calc_loss_of_triplet(a, p, n, margin=1.0))
+    assert approx_equal(loss, 1.0)
     assert approx_equal(d_p, torch.linalg.norm(a - p))
     assert approx_equal(d_n, torch.linalg.norm(a - n))
 
     loss_torch = TripletMarginLoss(margin=1.0)(a, p, n)
-    assert approx_equal(loss_torch, loss) and approx_equal(loss_torch, 1.0)
+    assert approx_equal(loss_torch, loss)
+    assert approx_equal(loss_torch, 1.0)
 
 
 def test_tripletloss_online_soft():
