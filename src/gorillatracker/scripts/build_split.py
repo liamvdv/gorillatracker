2fdc24cb6418dc9558599cacd2a142d515f08dbd
Rob2U <robert.weeke@student.hpi.uni-potsdam.de>
2023-12-06 14:30:42 +0000

    apply formatting

--- src/gorillatracker/scripts/build_split.py
+++ src/gorillatracker/scripts/build_split.py
@@ -96,13 +96,15 @@ def generate_split_save_metadata_cxl(
 
 
 if __name__ == "__main__":
-    bristol_split_dir = str(generate_split(
-        dataset="ground_truth/bristol/full_images",
-        mode="openset",
-        seed=69,
-        reid_factor_test=0,
-        reid_factor_val=0,
-    ))
+    bristol_split_dir = str(
+        generate_split(
+            dataset="ground_truth/bristol/full_images",
+            mode="openset",
+            seed=69,
+            reid_factor_test=0,
+            reid_factor_val=0,
+        )
+    )
     bristol_split_dir = str(os.path.abspath(bristol_split_dir))
     ensure_integrity_openset(bristol_split_dir)
 
