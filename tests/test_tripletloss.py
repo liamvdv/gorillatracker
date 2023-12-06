3d9fe17daf8c7dbc0e9382a094f683c2b46e313e
Rob2U <robert.weeke@student.hpi.uni-potsdam.de>
2023-12-06 15:19:20 +0000

    make mypy happy

--- tests/test_tripletloss.py
+++ tests/test_tripletloss.py
@@ -1,10 +1,10 @@
 import torch
 from torch.nn import TripletMarginLoss
+from typing import Tuple, Union
 
 from gorillatracker.triplet_loss import TripletLossOffline, TripletLossOnline
 
-
-def calc_loss_of_triplet(anchor, positive, negative, margin):
+def calc_loss_of_triplet(anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor, margin: float) -> torch.Tensor:
     loss = torch.relu(
         torch.linalg.vector_norm(anchor - positive) - torch.linalg.vector_norm(anchor - negative) + margin
     )
@@ -13,18 +13,18 @@ def calc_loss_of_triplet(anchor, positive, negative, margin):
     return loss
 
 
-def calc_loss_and_distance_of_triplet(anchor, positive, negative, margin):
+def calc_loss_and_distance_of_triplet(anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor, margin: float) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     loss = torch.relu(
         torch.linalg.vector_norm(anchor - positive) - torch.linalg.vector_norm(anchor - negative) + margin
     )
     return loss, torch.linalg.vector_norm(anchor - positive), torch.linalg.vector_norm(anchor - negative)
 
 
-def approx_equal(a, b, eps=1e-6):
+def approx_equal(a: torch.Tensor, b: torch.Tensor, eps: float = 1e-6) -> bool:
     return torch.abs(a - b) < eps
 
 
-def test_tripletloss_offline():
+def test_tripletloss_offline() -> None:
     a, p, n = torch.tensor([1.0]), torch.tensor([0.5]), torch.tensor([-1.0])
     labels = torch.tensor([0, 0, 1])
 
@@ -42,16 +42,16 @@ def test_tripletloss_offline():
     loss, d_p, d_n = triplet_loss(torch.stack([a, p, n]), labels)
 
     assert approx_equal(loss, calc_loss_of_triplet(a, p, n, margin=1.0))
-    assert approx_equal(loss, 1.0)
+    assert approx_equal(loss, torch.Tensor([1.0]))
     assert approx_equal(d_p, torch.linalg.norm(a - p))
     assert approx_equal(d_n, torch.linalg.norm(a - n))
 
     loss_torch = TripletMarginLoss(margin=1.0)(a, p, n)
     assert approx_equal(loss_torch, loss)
-    assert approx_equal(loss_torch, 1.0)
+    assert approx_equal(loss_torch, torch.Tensor([1.0]))
 
 
-def test_tripletloss_online_soft():
+def test_tripletloss_online_soft() -> None:
     a, ap, n, np = torch.tensor([3.0]), torch.tensor([0.5]), torch.tensor([0.5]), torch.tensor([-0.5])
     loss012, d_p012, d_n012 = calc_loss_and_distance_of_triplet(a, ap, n, margin=1.0)
     loss013, d_p013, d_n013 = calc_loss_and_distance_of_triplet(a, ap, np, margin=1.0)
@@ -76,7 +76,7 @@ def test_tripletloss_online_soft():
     assert approx_equal(distance_negative_manual, distance_negative_online_soft)
 
 
-def test_tripletloss_online_hard():
+def test_tripletloss_online_hard() -> None:
     a, ap, n, np = torch.tensor([0.3]), torch.tensor([1.0]), torch.tensor([0.25]), torch.tensor([-0.5])
     loss012, d_p012, d_n012 = calc_loss_and_distance_of_triplet(a, ap, n, margin=1.0)
     loss102, d_p102, d_n102 = calc_loss_and_distance_of_triplet(ap, a, n, margin=1.0)
@@ -97,7 +97,7 @@ def test_tripletloss_online_hard():
     assert approx_equal(distance_negative_manual, distance_negative_online_hard)
 
 
-def test_tripletloss_online_semi_hard():
+def test_tripletloss_online_semi_hard() -> None:
     a, ap, n, np = torch.tensor([0.3]), torch.tensor([1.0]), torch.tensor([0.25]), torch.tensor([0.5])
     loss102, d_p102, d_n102 = calc_loss_and_distance_of_triplet(ap, a, n, margin=1.0)
     loss231, d_p231, d_n231 = calc_loss_and_distance_of_triplet(n, np, ap, margin=1.0)
