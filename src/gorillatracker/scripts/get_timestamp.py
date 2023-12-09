63bdb0e16cc8e56257351491465bfddaeca934e3
Joscha Schroff <joscha.schroff@gmail.com>
2023-12-09 16:40:30 +0000

    add comment

--- src/gorillatracker/scripts/get_timestamp.py
+++ src/gorillatracker/scripts/get_timestamp.py
@@ -1,6 +1,7 @@
 import cv2
 import easyocr
 
+# consider putting this in an other python file for video detection
 def get_time_stamp(video_path: str) -> str:
     """
     Extracts the time stamp from the video file.
