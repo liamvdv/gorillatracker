1532583c06b8a8d696c3d72250eabac366b94295
Bennet Kampe <bennet.kampe@student.hpi.de>
2023-12-15 14:14:12 +0000

    loading project name from config yaml, lots of assertions for errorchecking because why not

--- init_sweep.py
+++ init_sweep.py
@@ -1,11 +1,27 @@
+import os
+from typing import Dict, Union, cast
+
+import yaml
+
 from wandb import agent, sweep
 
 # Set your default config
 config_path = "./cfgs/resnet18_cxl.yml"
+# Assuming `config_path` is the path to your YAML config file
+assert os.path.isfile(config_path), f"Config file not found at {config_path}"
+assert config_path.endswith(".yml") or config_path.endswith(".yaml"), "Config file must be YAML"
+config_dict: Dict[str, Union[str, int, float, bool]] = dict()
+with open(config_path, "r") as file:
+    config_dict = yaml.safe_load(file)
+
+project_name = cast(str, config_dict["project_name"])
+
+assert len(project_name.split("-")) >= 4, "Project name must be of the form <Function>-<Backbone>-<Dataset>-<Set-Type>"
+
 # Define your sweep configuration
 sweep_config = {
     "program": "./train.py",  # Note: not the sweep file, but the training script
-    "name": "test",
+    "name": project_name,
     "method": "grid",  # Specify the search method (random search in this case)
     "metric": {"goal": "minimize", "name": "val/loss"},  # Specify the metric to optimize
     "parameters": {
