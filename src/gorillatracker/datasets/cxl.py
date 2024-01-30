8e6a0a54d5313d35a8b6bced3e3e714e0968b000
Emirhan <emirhan.dogan404@gmail.com>
2024-01-30 19:44:25 +0000

    local calculation of embeddings and use of dataset

--- src/gorillatracker/datasets/cxl.py
+++ src/gorillatracker/datasets/cxl.py
@@ -55,9 +55,12 @@ class CXLDataset(Dataset[Tuple[Image.Image, Label]]):
         # new
         labels_string = [label for _, label in samples]
         labels_int = cast_label_to_int(labels_string)
+        self.mapping = dict(zip(labels_int, labels_string))
         self.samples = list(zip([path for path, _ in samples], labels_int))
 
         self.transform = transform
+
+        self.partition = partition
 
     def __len__(self) -> int:
         return len(self.samples)
@@ -67,16 +70,20 @@ class CXLDataset(Dataset[Tuple[Image.Image, Label]]):
         img = Image.open(img_path)
         if self.transform:
             img = self.transform(img)
+
+        # save img
+        # img2 = transforms.ToPILImage()(img)
+        # img2.save(f"img_{self.partition}.png")
+
         return img, label
 
     @classmethod
     def get_transforms(cls) -> gtypes.Transform:
         return transforms.Compose(
             [
-                SquarePad(),
                 # Uniform input, you may choose higher/lower sizes.
-                transforms.Resize(224),
-                transforms.ToTensor(),
+                SquarePad(),
+                # transforms.ToTensor(),
             ]
         )
 
