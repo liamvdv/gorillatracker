36f58b8e5a9b2c39a452b10eeb2ddcbde77fedf0
Rob2U <robert.weeke@student.hpi.uni-potsdam.de>
2024-02-01 09:35:35 +0000

    unrefactored working version

--- src/gorillatracker/utils/load.py
+++ src/gorillatracker/utils/load.py
@@ -1,108 +1,97 @@
-from typing import Any
+from typing import Any, Callable
 
 import torch
+import torchvision.transforms as transforms
 import wandb
 import pandas as pd
 from tqdm import tqdm
 
+from gorillatracker.datasets.cxl import CXLDataset
+from gorillatracker.model import BaseModule
+
 
 # load model from wandb artifact
-def load_model_from_artifact(artifact: wandb.Artifact, model: Any, device: str) -> Any: # TODO
-    model_state_dict = artifact.get('model_state_dict').state_dict()
+def load_model_from_wandb(wandb_fullname, model_cls: Any, embedding_size, device: str) -> Any:  # TODO
+    wandb.login()
+    wandb.init(mode="disabled")
+    api = wandb.Api()
+
+    artifact = api.artifact(
+        wandb_fullname,  # your artifact name
+        type="model",
+    )
+    artifact_dir = artifact.download()
+    model = artifact_dir + "/model.ckpt"
+    checkpoint = torch.load(model, map_location=torch.device("cpu"))
+    model_state_dict = checkpoint["state_dict"]
+
+    model = model_cls(
+        embedding_size=embedding_size,
+    )
+
+    if not hasattr(model_state_dict, "loss_module_train.prototypes") or not hasattr(
+        model_state_dict, "loss_module_val.prototypes"
+    ):  # necessary because arcface loss also saves prototypes
+        model.loss_module_train.prototypes = torch.nn.Parameter(model_state_dict["loss_module_train.prototypes"])
+        model.loss_module_val.prototypes = torch.nn.Parameter(model_state_dict["loss_module_val.prototypes"])
+
     model.load_state_dict(model_state_dict)
     model.to(device)
+    model.eval()
     return model
 
+
 # generate embeddings (dataframe) from model and dataset
-def generate_embeddings(model, dataset, device): # TODO
-    model.eval()
+def generate_embeddings(model, dataset, device):  # TODO
     embeddings = []
-    df = pd.DataFrame(columns=['embedding', 'label', 'input', 'label_string'])
+    df = pd.DataFrame(columns=["embedding", "label", "input", "label_string"])
     with torch.no_grad():
-        for batch_inputs, batch_labels in tqdm(dataset): # TODO
+        print("Generating embeddings...")
+        for imgs, labels in tqdm(dataset):
+            if isinstance(imgs, torch.Tensor):  # if single image is passe wrap it in list
+                imgs = [imgs]
+                labels = [labels]
+            batch_inputs = torch.stack(imgs)
+            if batch_inputs.shape[0] != 1:
+                batch_inputs = batch_inputs.unsqueeze(1)
             batch_inputs = batch_inputs.to(device)
             embeddings = model(batch_inputs)
-            for i in range(len(batch_inputs)):
-                df.append({'embedding': embeddings[i], 'label': batch_labels[i], 'input': batch_inputs[i], 'label_string': dataset.mapping[batch_labels[i]]}, ignore_index=True, inplace=True)
+
+            for i in range(len(imgs)):
+                input_img = transforms.ToPILImage()(batch_inputs[i].cpu())
+                df = pd.concat(
+                    [
+                        df,
+                        pd.DataFrame(
+                            {
+                                "embedding": [embeddings[i]],
+                                "label": [labels[i]],
+                                "input": [input_img],
+                                "label_string": [dataset.mapping[labels[i]]],
+                            }
+                        ),
+                    ]
+                )
     df.reset_index(drop=False, inplace=True)
     return df
 
-""" Code from my notebook
 
-wandb.login()
-wandb.init(mode="disabled")
-api = wandb.Api()
+def get_dataset(
+    partition: str = "val",
+    data_dir: str = "/workspaces/gorillatracker/data/splits/ground_truth-cxl-face_images-openset-reid-val-0-test-0-mintraincount-3-seed-42-train-50-val-25-test-25",
+    transform: Callable = None,
+    model: BaseModule = BaseModule(),
+):
+    if transform is None:
+        transform = transforms.Compose(
+            [
+                CXLDataset.get_transforms(),
+                model.get_tensor_transforms(),
+            ]
+        )
 
-artifact = api.artifact(
-    "gorillas/Embedding-ALL-SPAC-Open/model-3ag1c2vf:v1",  # your artifact name
-    type="model",
-)
-artifact_dir = artifact.download()
-model = artifact_dir + "/model.ckpt"
-
-# load model
-checkpoint = torch.load(model, map_location=torch.device("cpu"))
-
-model = EfficientNetV2Wrapper(  # switch this with the model you want to use
-    model_name_or_path="EfficientNetV2_Large",
-    from_scratch=False,
-    loss_mode="softmax/arcface",
-    weight_decay=0.001,
-    lr_schedule="cosine",
-    warmup_mode="cosine",
-    warmup_epochs=10,
-    max_epochs=100,
-    initial_lr=0.01,
-    start_lr=0.01,
-    end_lr=0.0001,
-    beta1=0.9,
-    beta2=0.999,
-    embedding_size=128,
-)
-# the following lines are necessary to load a model that was trained with arcface (the prototypes are saved in the state dict)
-model.loss_module_train.prototypes = torch.nn.Parameter(checkpoint["state_dict"]["loss_module_train.prototypes"])
-model.loss_module_val.prototypes = torch.nn.Parameter(checkpoint["state_dict"]["loss_module_val.prototypes"])
-
-model.load_state_dict(checkpoint["state_dict"])
-model.eval()
-
-# generate table that contains labels and images and embeddings
-df = pd.DataFrame(columns=["label", "image", "embedding"])
-dataset = CXLDataset(
-    data_dir="/workspaces/gorillatracker/data/splits/ground_truth-cxl-face_images-openset-reid-val-0-test-0-mintraincount-3-seed-42-train-50-val-25-test-25",
-    partition="val",
-    transform=transforms.Compose(  # use the transforms that were used for the model (except of course data augmentations)
-        [
-            SquarePad(),
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-        ]
-    ),
-)
-
-for i in range(len(dataset)):
-    image_tensor, label = dataset[i]
-    label_string = dataset.mapping[label]
-    image = transforms.ToPILImage()(image_tensor)
-    image_tensor = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.228, 0.224, 0.225])(
-        image_tensor
-    )  # if your model was trained with normalization, you need to normalize the images here as well
-    embedding = model(image_tensor.unsqueeze(0))
-    df = pd.concat(
-        [
-            df,
-            pd.DataFrame(
-                {
-                    "label_string": [label_string],
-                    "label": [label],
-                    "image": [image],
-                    "embedding": [embedding[0].detach().numpy()],
-                }
-            ),
-        ]
+    return CXLDataset(
+        data_dir=data_dir,
+        partition=partition,
+        transform=transform,
     )
-
-    if i % 10 == 0:
-        print(f"\rprocessed {i} images")
-df = df.reset_index(drop=False)
-"""
