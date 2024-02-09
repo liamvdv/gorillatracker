81fdbd8bab71d4075637371754939368746a2480
Rob2U <robert.weeke@student.hpi.uni-potsdam.de>
2024-02-09 11:41:14 +0000

    quick fix

--- src/gorillatracker/metrics.py
+++ src/gorillatracker/metrics.py
@@ -113,7 +113,7 @@ class LogEmbeddingsToWandbCallback(L.Callback):
             train_labels=train_labels,
         )
         # clear the table where the embeddings are stored
-        pl_module.embeddings_table = pd.DataFrame(columns=pl_module.embeddings_table_columns)  # reset embeddings table
+        # pl_module.embeddings_table = pd.DataFrame(columns=pl_module.embeddings_table_columns)  # rese t embeddings table
 
     def on_train_epoch_start(self, trainer: L.Trainer, pl_module: L.LightningModule) -> None:
         log_train_images_to_wandb(self.run, trainer, n_samples=1)
