1697fa3c9cbfe722842926d95ec67d6d8e7a0d4e
Kajo Kratzenstein <kajo.kratzenstein@student.hpi.de>
2024-02-12 15:03:29 +0000

    up

--- platform/gather_labels.py
+++ platform/gather_labels.py
@@ -2,17 +2,47 @@ import faiss
 import torch
 import gorillatracker.utils.embedding_generator as eg
 import numpy as np
+import pandas as pd
+import pathlib
+import hashlib
 
-from typing import List
+from typing import List, Optional
 
 
 class LabelGatherer:
-    def __init__(self) -> None:
-        self.embeddings_df = eg.read_embeddings_from_disk("./data/embeddings/swin_v2_cxl_open.pkl")
+    def __init__(
+        self,
+        model_from_run: str,
+        use_cache: bool = True,
+        data_dir: Optional[str] = None,
+        dataset_class: Optional[str] = None,
+    ) -> None:
+        self.cache_destination = "./data/embeddings"
+        self.embeddings_df = self._get_embeddings(model_from_run, use_cache, data_dir, dataset_class)
         self.index = self._get_faiss_index(self.embeddings_df["embedding"].tolist())
 
+    def _get_embeddings(
+        self,
+        model_from_run: str,
+        use_cache: bool = True,
+        data_dir: Optional[str] = None,
+        dataset_class: Optional[str] = None,
+    ) -> pd.DataFrame:
+        combined_name_bytes = (model_from_run + str(data_dir)).encode()
+        embeddings_on_disk_path = pathlib.Path(self.cache_destination) / (
+            hashlib.sha256(combined_name_bytes).hexdigest() + ".pkl"
+        )
+        if use_cache and embeddings_on_disk_path.exists():
+            return eg.read_embeddings_from_disk(str(embeddings_on_disk_path))
+
+        return eg.generate_embeddings_from_run(
+            run_url=model_from_run,
+            outpath=str(embeddings_on_disk_path if use_cache else "-"),
+            data_dir=data_dir,
+            dataset_class=dataset_class,
+        )
+
     def _get_faiss_index(self, embeddings: list[torch.Tensor]) -> faiss.IndexFlatL2:
-        """Returns the labels of the embeddings"""
         embeddings = np.array(embeddings)  # type: ignore
         faiss.normalize_L2(embeddings)
 
@@ -23,19 +53,21 @@ class LabelGatherer:
         return index
 
     def _get_label_for_embedding(self, embedding: torch.Tensor, k: int = 1) -> List[str]:
-        """Returns the label for the given embedding"""
         embedding = embedding.unsqueeze(0).numpy()
         faiss.normalize_L2(embedding)
         _, I = self.index.search(embedding, k)
         return self.embeddings_df.iloc[I[0][0]]["label_string"]
 
     def get_labels_for_embeddings(self, embeddings: dict) -> set[str]:
-        """Returns the labels for the given embeddings"""
         labels = []
         for embedding_value in embeddings.values():
             labels.append(self._get_label_for_embedding(embedding_value["embedding"]))
         return set(labels)
 
 
-labl = LabelGatherer()
-print(labl.get_labels_for_embeddings({"1": {"embedding": torch.rand(256)}}))
+if __name__ == "__main__":
+    labl = LabelGatherer(
+        model_from_run="https://wandb.ai/gorillas/Embedding-SwinV2-CXL-Open/runs/69ok0oyl"
+    )
+    print(labl.get_labels_for_embeddings({"1": {"embedding": torch.rand(256)}}))
+
