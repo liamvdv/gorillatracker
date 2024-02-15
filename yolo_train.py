502af0f007d26067b02d10f5a79514e409a9bc77
memben <ben.meyermeisel@gmail.com>
2024-02-15 21:18:05 +0000

    Add YOLO detection configuration files for body and face detection

--- yolo_train.py
+++ yolo_train.py
@@ -3,32 +3,26 @@ import time
 
 from ultralytics import YOLO
 
-WANDB_PROJECT = "Detection-YoloV8-CXLBody"
+WANDB_PROJECT = "Detection-YoloV8-CXLBody-ClosedSet"
 WANDB_ENTITY = "gorillas"
 
 
-model_paths = {
-    "yolov8n": "./models/yolov8n.pt",
-    "yolov8m": "./models/yolov8m.pt",
-    "yolov8x": "./models/yolov8x.pt",
-}
-
-
 def train(
-    model_name: str,
+    model_path: str,
     training_name: str,
-    data: str = "./gorilla.yaml",
+    data: str,
     epochs: int = 100,
     batch_size: int = -1,
     patience: int = 40,
 ) -> None:
-    model = YOLO(model_paths[model_name])
-    training_name = f"{training_name}-{model_name}"
+    model = YOLO(model_path)
+    training_name = f"{training_name}"
     model.train(
         project=WANDB_PROJECT, name=training_name, data=data, epochs=epochs, batch=batch_size, patience=patience
     )
     shutil.move(WANDB_PROJECT, f"logs/{WANDB_PROJECT}-{training_name}-{time.strftime('%Y-%m-%d-%H-%M-%S')}")
 
 
-# if __name__ == "__main__":
-#     train("yolov8x", "#36-max-power-test", epochs=200, patience=50)
+if __name__ == "__main__":
+    # train("/workspaces/gorillatracker/models/yolov8n_gorillaface_pkm2bzis.pt", "new-face-data", data="/workspaces/gorillatracker/cfgs/yolo_detection_face.yaml", epochs=200, patience=50)
+    train("/workspaces/gorillatracker/yolov8n.pt", "#198-body-fmc", "/workspaces/gorillatracker/cfgs/yolo_detection_body.yaml", epochs=200, patience=50)
