4c2226a6db369a2f0345dac706d59b1f70674da7
memben <ben.meyermeisel@gmail.com>
2024-02-20 23:15:51 +0000

    add new datasets

--- yolo_train.py
+++ yolo_train.py
@@ -3,7 +3,7 @@ import time
 
 from ultralytics import YOLO
 
-WANDB_PROJECT = "Detection-YoloV8-DATASET-ClosedSet"
+WANDB_PROJECT = "Detection-YoloV8-CXLBody-ClosedSet"
 WANDB_ENTITY = "gorillas"
 
 
@@ -17,7 +17,7 @@ def train(
 ) -> None:
     training_name = f"{training_name}"
     model.train(
-        project=WANDB_PROJECT, name=training_name, data=data, epochs=epochs, batch=batch_size, patience=patience
+        project=WANDB_PROJECT, name=training_name, data=data, epochs=epochs, batch=batch_size, patience=patience, single_cls=True
     )
     shutil.move(WANDB_PROJECT, f"logs/{WANDB_PROJECT}-{training_name}-{time.strftime('%Y-%m-%d-%H-%M-%S')}")
 
@@ -28,12 +28,17 @@ def tune(
     data: str,
     iterations: int = 30,
 ) -> None:
-    model.tune(project=WANDB_PROJECT, name=training_name, data=data, iterations=iterations, epochs=150, batch=-1) # -1 autobatch
+    model.tune(project=WANDB_PROJECT, name=training_name, data=data, iterations=iterations, epochs=200, batch=-1, single_cls=True) # -1 autobatch
     shutil.move(WANDB_PROJECT, f"logs/{WANDB_PROJECT}-{training_name}-{time.strftime('%Y-%m-%d-%H-%M-%S')}")
 
 
 if __name__ == "__main__":
-    model = YOLO("path/to/your/yolov8n.pt")
-    run = "#your-run-name"
-    data = "path/to/your/yolo_detection.yaml"
-    tune(model, run, data, iterations=10)
+    # model = YOLO("path/to/your/yolov8n.pt")
+    # run = "#your-run-name"
+    # data = "path/to/your/yolo_detection.yaml"    
+    model = YOLO("/workspaces/gorillatracker/yolov8n.pt")
+    # run = "#198-tune-gorilla-silverback-yolov8n"
+    run = "#198-train-gorilla-yolov8n"
+    data = "/workspaces/gorillatracker/cfgs/yolo_detection_body_gs.yaml"
+    # tune(model, run, data, iterations=5)
+    train(model, run, data, epochs=200, batch_size=32, patience=40)
