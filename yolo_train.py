2bcf1895b72b652672f9743e5104c90e0c71968c
memben <ben.meyermeisel@gmail.com>
2024-02-24 08:51:36 +0000

    addyolo custom sweeping

--- yolo_train.py
+++ yolo_train.py
@@ -1,44 +1,46 @@
 import shutil
 import time
+from typing import Optional
 
+import typer
 from ultralytics import YOLO
 
-WANDB_PROJECT = "Detection-YoloV8-CXLBody-ClosedSet"
+app = typer.Typer()
+
+WANDB_PROJECT = "Detection-YoloV8-CXLFace-ClosedSet"
 WANDB_ENTITY = "gorillas"
 
 
+@app.command()
 def train(
-    model: YOLO,
+    model_path: str,
     training_name: str,
     data: str,
     epochs: int = 100,
-    batch_size: int = -1, # -1 autobatch
+    batch_size: int = -1,  # -1 for autobatch
     patience: int = 40,
 ) -> None:
+    model = YOLO(model_path)
     training_name = f"{training_name}"
+    print(f"Training {training_name}")
     model.train(
         project=WANDB_PROJECT, name=training_name, data=data, epochs=epochs, batch=batch_size, patience=patience, single_cls=True
     )
+    print(f"Training of {training_name} finished")
     shutil.move(WANDB_PROJECT, f"logs/{WANDB_PROJECT}-{training_name}-{time.strftime('%Y-%m-%d-%H-%M-%S')}")
 
 
+@app.command()
 def tune(
-    model: YOLO,
+    model_path: str,
     training_name: str,
     data: str,
     iterations: int = 30,
 ) -> None:
-    model.tune(project=WANDB_PROJECT, name=training_name, data=data, iterations=iterations, epochs=200, batch=-1, single_cls=True) # -1 autobatch
+    model = YOLO(model_path)
+    model.tune(project=WANDB_PROJECT, name=training_name, data=data, iterations=iterations, epochs=200, single_cls=True)
     shutil.move(WANDB_PROJECT, f"logs/{WANDB_PROJECT}-{training_name}-{time.strftime('%Y-%m-%d-%H-%M-%S')}")
 
 
 if __name__ == "__main__":
-    # model = YOLO("path/to/your/yolov8n.pt")
-    # run = "#your-run-name"
-    # data = "path/to/your/yolo_detection.yaml"    
-    model = YOLO("/workspaces/gorillatracker/yolov8n.pt")
-    # run = "#198-tune-gorilla-silverback-yolov8n"
-    run = "#198-train-gorilla-yolov8n"
-    data = "/workspaces/gorillatracker/cfgs/yolo_detection_body_gs.yaml"
-    # tune(model, run, data, iterations=5)
-    train(model, run, data, epochs=200, batch_size=32, patience=40)
+    app()
