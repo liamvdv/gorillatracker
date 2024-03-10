e49ebe9d23fcf5178f540dc9b139ef6dd572a1bc
memben <ben.meyermeisel@gmail.com>
2024-03-10 22:38:00 +0000

    add updated models

--- src/gorillatracker/ssl_pipeline/models.py
+++ src/gorillatracker/ssl_pipeline/models.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
-import datetime
 import enum
-from typing import Optional
+from datetime import datetime, timedelta
 
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
-class RelationshipType(enum.Enum):
-    NEGATIVE = -1
-    POSITIVE = 1
+class VideoRelationshipType(enum.Enum):
+    NEGATIVE = -1  # Implies that all Trackings in the left video are not in the right video and vice versa
+    POSITIVE = 1  # Implies that one or more Trackings could be in both videos
+
+
+class TrackingRelationshipType(enum.Enum):
+    NEGATIVE = -1  # Implies that the Trackings are not the same
+    POSITIVE = 1  # Implies that the Trackings are the same (animal)
 
 
 class Base(DeclarativeBase):
@@ -18,134 +22,161 @@ class Base(DeclarativeBase):
 
 
 class Camera(Base):
-    __tablename__ = "camera"
+    __tablename__ = "Camera"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String(30))
+    camera_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True)
     latitude: Mapped[float]
     longitude: Mapped[float]
 
-    video_clips: Mapped[Optional[list[VideoClip]]] = relationship(back_populates="camera")
+    videos: Mapped[list[Video]] = relationship(back_populates="camera", cascade="all, delete-orphan")
 
     def __repr__(self) -> str:
-        return f"Camera(id={self.id}, name={self.name}, latitude={self.latitude}, longitude={self.longitude})"
+        return f"Camera(id={self.camera_id}, name={self.name}, latitude={self.latitude}, longitude={self.longitude})"
 
 
-class VideoClip(Base):
-    __tablename__ = "video_clip"
+class Video(Base):
+    __tablename__ = "Video"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    file_path: Mapped[str] = mapped_column(String(100))  # video_data/example.mp4
-    camera_id: Mapped[int] = mapped_column(ForeignKey("camera.id"))
-    start_time: Mapped[datetime.datetime]
+    video_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    filename: Mapped[str] = mapped_column(String(255), unique=True)
+    camera_id: Mapped[int] = mapped_column(ForeignKey("Camera.camera_id"))
+    start_time: Mapped[datetime]
+    width: Mapped[int]
+    height: Mapped[int]
     fps: Mapped[int]
     frames: Mapped[int]
-    width = Mapped[int]
-    height = Mapped[int]
     grayscale: Mapped[bool]
 
-    @property
-    def duration(self) -> datetime.timedelta:
-        return datetime.timedelta(seconds=self.frames / self.fps)
+    camera: Mapped[Camera] = relationship("Camera", back_populates="videos")
+    video_features: Mapped[list[VideoFeature]] = relationship(back_populates="video", cascade="all, delete-orphan")
+    trackings: Mapped[list[Tracking]] = relationship(back_populates="video", cascade="all, delete-orphan")
 
-    camera: Mapped[Camera] = relationship(back_populates="video_clips")
-    tracked_animals: Mapped[Optional[list[TrackedAnimal]]] = relationship(back_populates="video")
+    @property
+    def duration(self) -> timedelta:
+        return timedelta(seconds=self.frames / self.fps)
 
     def __repr__(self) -> str:
-        return f"VideoClip(id={self.id}, camera_id={self.camera_id}, start_time={self.start_time}, fps={self.fps}, frames={self.frames}, gray={self.grayscale})"
+        return f"Video(id={self.video_id}, filename={self.filename}, camera_id={self.camera_id}, start_time={self.start_time}, fps={self.fps}, frames={self.frames}, gray={self.grayscale})"
+
+
+class VideoFeature(Base):
+    __tablename__ = "VideoFeature"
+
+    feature_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    video_id: Mapped[int] = mapped_column(ForeignKey("Video.video_id"))
+    type: Mapped[str] = mapped_column(String(255))
+    value: Mapped[str] = mapped_column(String(255))
+
+    video: Mapped[Video] = relationship(back_populates="video_features")
+
+    def __repr__(self) -> str:
+        return f"VideoFeature(id={self.feature_id}, video_id={self.video_id}, type={self.type}, value={self.value})"
+
+
+class Tracking(Base):
+    """Represent a continuous tracking of an animal in a video.
+
+    A tracking is a sequence of frames in which an animal is tracked.
+    The tracking is represented by a list of TrackingFrameFeatures,
+    which are the features of the animal in each frame.
+
+    There can be multiple trackings of the same animal.
+    """
+
+    __tablename__ = "Tracking"
+
+    tracking_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    video_id: Mapped[int] = mapped_column(ForeignKey("Video.video_id"))
+
+    video: Mapped[Video] = relationship(back_populates="trackings")
+    tracking_features: Mapped[list[TrackingFeature]] = relationship(
+        back_populates="tracking", cascade="all, delete-orphan"
+    )
+    tracking_frame_features: Mapped[list[TrackingFrameFeature]] = relationship(
+        back_populates="tracking", cascade="all, delete-orphan"
+    )
+
+    @property
+    def tracking_duration(self) -> timedelta:
+        fps = self.video.fps
+        start_frame = min(self.tracking_frame_features, key=lambda x: x.frame_nr).frame_nr
+        end_frame = max(self.tracking_frame_features, key=lambda x: x.frame_nr).frame_nr
+        return timedelta(seconds=(end_frame - start_frame) / fps)
+
+    def __repr__(self) -> str:
+        return f"Tracking(id={self.tracking_id}, video_id={self.video_id})"
+
+
+class TrackingFeature(Base):
+    __tablename__ = "TrackingFeature"
+
+    tracking_feature_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tracking_id: Mapped[int] = mapped_column(ForeignKey("Tracking.tracking_id"))
+    type: Mapped[str] = mapped_column(String(255))
+    value: Mapped[str] = mapped_column(String(255))
+
+    tracking: Mapped[Tracking] = relationship(back_populates="tracking_features")
+
+    def __repr__(self) -> str:
+        return f"TrackingFeature(id={self.tracking_feature_id}, tracking_id={self.tracking_id}, type={self.type}, value={self.value})"
+
+
+class TrackingFrameFeature(Base):
+    """Represent the detected bounding box of a tracking feature in a frame."""
+
+    __tablename__ = "TrackingFrameFeature"
+
+    tracking_frame_feature_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tracking_id: Mapped[int] = mapped_column(ForeignKey("Tracking.tracking_id"))
+    frame_nr: Mapped[int]
+    bbox_x_center: Mapped[float]
+    bbox_y_center: Mapped[float]
+    bbox_width: Mapped[float]
+    bbox_height: Mapped[float]
+    confidence: Mapped[float]
+    type: Mapped[str] = mapped_column(String(255))
+
+    tracking: Mapped["Tracking"] = relationship(back_populates="tracking_frame_features")
+
+    __table_args__ = (UniqueConstraint("tracking_id", "frame_nr", "type"),)
+
+    def __repr__(self) -> str:
+        return f"TrackingFrameFeature(id={self.tracking_frame_feature_id}, tracking_id={self.tracking_id}, frame_nr={self.frame_nr}, bbox_x_center={self.bbox_x_center}, bbox_y_center={self.bbox_y_center}, bbox_width={self.bbox_width}, bbox_height={self.bbox_height}, confidence={self.confidence}, type={self.type})"
 
 
 class VideoRelationship(Base):
-    __tablename__ = "video_relationship"
+    __tablename__ = "VideoRelationship"
 
-    video_id_1: Mapped[int] = mapped_column(ForeignKey("video_clip.id"), primary_key=True)
-    video_id_2: Mapped[int] = mapped_column(ForeignKey("video_clip.id"), primary_key=True)
-    relationship_type: Mapped[RelationshipType]
+    video_relationship_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    left_video_id: Mapped[int] = mapped_column(ForeignKey("Video.video_id"))
+    right_video_id: Mapped[int] = mapped_column(ForeignKey("Video.video_id"))
+    edge: Mapped[VideoRelationshipType]
+    reason: Mapped[str] = mapped_column(String(255))
+    created_by: Mapped[str] = mapped_column(String(255))
 
-    video_1: Mapped[VideoClip] = relationship(foreign_keys=[video_id_1])
-    video_2: Mapped[VideoClip] = relationship(foreign_keys=[video_id_2])
-
-    def __repr__(self) -> str:
-        return f"VideoRelationship(video_id_1={self.video_id_1}, video_id_2={self.video_id_2}, relationship_type={self.relationship_type})"
-
-
-class Animal(Base):
-    __tablename__ = "animal"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String(30))
-
-    tracked_animals: Mapped[Optional[list[TrackedAnimal]]] = relationship(back_populates="animal")
-
-
-class TrackedAnimal(Base):
-    __tablename__ = "tracked_animal"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    video_id: Mapped[int] = mapped_column(ForeignKey("video_clip.id"))
-    animal_type: Mapped[int] = mapped_column(ForeignKey("animal.id"))
-
-    animal: Mapped[Animal] = relationship(back_populates="tracked_animals")
-    video: Mapped[VideoClip] = relationship(back_populates="tracked_animals")
-    trackings: Mapped[list[Tracking]] = relationship(back_populates="tracked_animal")
-
-    @property
-    def tracking_duration(self) -> datetime.timedelta:
-        fps = self.video.fps
-        start_frame = min(self.trackings, key=lambda x: x.frame_number).frame_number
-        end_frame = max(self.trackings, key=lambda x: x.frame_number).frame_number
-        return datetime.timedelta(seconds=(end_frame - start_frame) / fps)
+    left_video: Mapped[Video] = relationship(foreign_keys=[left_video_id])
+    right_video: Mapped[Video] = relationship(foreign_keys=[right_video_id])
 
     def __repr__(self) -> str:
-        return f"TrackedAnimal(id={self.id}, video_id={self.video_id}, animal_type={self.animal_type})"
+        return f"VideoRelationship(id={self.video_relationship_id}, left_video_id={self.left_video_id}, right_video_id={self.right_video_id}, edge={self.edge}, reason={self.reason}, created_by={self.created_by})"
 
 
-class BoundingBoxMixin:
-    x_center: Mapped[float]
-    y_center: Mapped[float]
-    width: Mapped[float]
-    height: Mapped[float]
-    confidence: Mapped[float]
+class TrackingRelationship(Base):
+    __tablename__ = "TrackingRelationship"
 
-    def __repr__(self) -> str:
-        return f"BoundingBox(x_center={self.x_center}, y_center={self.y_center}, width={self.width}, height={self.height}, confidence={self.confidence})"
+    tracking_relationship_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    left_video_id: Mapped[int] = mapped_column(ForeignKey("Tracking.tracking_id"))
+    right_video_id: Mapped[int] = mapped_column(ForeignKey("Tracking.tracking_id"))
+    edge: Mapped[TrackingRelationshipType]
+    reason: Mapped[str] = mapped_column(String(255))
+    created_by: Mapped[str] = mapped_column(String(255))
 
-
-class Tracking(Base, BoundingBoxMixin):
-    __tablename__ = "tracking"
-
-    tracked_animal_id: Mapped[int] = mapped_column(ForeignKey("tracked_animal.id"), primary_key=True)
-    frame_number: Mapped[int] = mapped_column(primary_key=True)
-
-    tracked_animal: Mapped[TrackedAnimal] = relationship(back_populates="trackings")
-
-    # TODO(@liamvdv): HELP
-    # feature_trackings: Mapped[Optional[list[FeatureTracking]]] = relationship(back_populates="tracking", primaryjoin="Tracking.tracked_animal_id == FeatureTracking.tracked_animal_id and Tracking.frame_number == FeatureTracking.frame_number", foreign_keys=[tracked_animal_id, frame_number])
-    def __repr__(self) -> str:
-        return f"Tracking(tracked_animal_id={self.tracked_animal_id}, frame_number={self.frame_number}), {super().__repr__()}"
-
-
-class Feature(Base):
-    __tablename__ = "feature"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    feature_name: Mapped[str] = mapped_column(String(30))
+    left_tracking: Mapped[Tracking] = relationship(foreign_keys=[left_video_id])
+    right_tracking: Mapped[Tracking] = relationship(foreign_keys=[right_video_id])
 
     def __repr__(self) -> str:
-        return f"Feature(id={self.id}, feature_name={self.feature_name})"
-
-
-class FeatureTracking(Base, BoundingBoxMixin):
-    __tablename__ = "feature_tracking"
-
-    tracked_animal_id: Mapped[int] = mapped_column(ForeignKey("tracking.tracked_animal_id"), primary_key=True)
-    frame_number: Mapped[int] = mapped_column(ForeignKey("tracking.frame_number"), primary_key=True)
-    feature_id: Mapped[int] = mapped_column(ForeignKey("feature.id"), primary_key=True)
-
-    # tracking: Mapped[Tracking] = relationship(back_populates="feature_tracking")
-
-    def __repr__(self) -> str:
-        return f"FeatureTracking(tracked_animal_id={self.tracked_animal_id}, frame_number={self.frame_number}), {super().__repr__()}"
+        return f"TrackingRelationship(id={self.tracking_relationship_id}, left_video_id={self.left_video_id}, right_video_id={self.right_video_id}, edge={self.edge}, reason={self.reason}, created_by={self.created_by})"
 
 
 if __name__ == "__main__":
