460015eed977e7f34b20450268affb6fb11c875b
memben <ben.meyermeisel@gmail.com>
2024-03-15 23:34:00 +0000

    continue refactor

--- src/gorillatracker/ssl_pipeline/video_feature_mapper.py
+++ src/gorillatracker/ssl_pipeline/video_feature_mapper.py
@@ -11,6 +11,7 @@ from gorillatracker.ssl_pipeline.correlators import Correlator
 from gorillatracker.ssl_pipeline.helpers import (
     AssociatedBoundingBox,
     BoundingBox,
+    TrackedFrame,
     load_tracked_frames,
     load_video_tracking,
 )
@@ -20,24 +21,20 @@ log = logging.getLogger(__name__)
 
 DEBUG = Literal["INTRODUCING UNDEFINED STATE, ACCEPTING DANGER", None]
 """
-Read carefully:
-You should never ever use this in production.
-Only use this for debugging purposes.
----
-The system is designed to have referential integrity.
-However, to visualize the unresolved boxes, we need to add them to the database.
-Thus we introduce a dummy tracking with a negative tracking_id (which is the video_id negated).
-This is a dangerous operation, as it introduces an undefined state in the database.
-It should work for the first part of the pipeline (tracking and correlating and visualization).
-To allow multiple TrackingFrameFeatures we need to deactivate the unique constraint on the TrackingFrameFeature table.
-`(UniqueConstraint("tracking_id", "frame_nr", "type")`.
+Important: This debug setting is strictly for debugging and should not be used in production environments. 
+It temporarily allows the addition of dummy trackings with negative tracking_ids (negated video_id) to visualize unresolved boxes, 
+thus breaching referential integrity. This is meant only for early pipeline stages (tracking, correlating, visualization). 
+Also, it necessitates disabling the TrackingFrameFeature table's unique constraint on ("tracking_id", "frame_nr", "type").
 
-Recovery: Delete all Tracking with a negative tracking_id.
+To revert any changes made using this in production, delete all Tracking entries with negative tracking_id.
 """
 
 
-def get_id_to_tracking(trackings: list[Tracking]) -> dict[int, Tracking]:
-    return {tracking.tracking_id: tracking for tracking in trackings}
+def convert_to_associated_bbox(tracked_frame: TrackedFrame) -> list[AssociatedBoundingBox]:
+    return [
+        AssociatedBoundingBox(feature.tracking_id, BoundingBox.from_tracking_frame_feature(feature))
+        for feature in tracked_frame.frame_features
+    ]
 
 
 def process_detections(prediction: results.Results, video_tracking: Video) -> list[BoundingBox]:
@@ -49,22 +46,25 @@ def process_detections(prediction: results.Results, video_tracking: Video) -> li
 
 
 def add_unresolved_boxes_for_debugging(
-    session: Session, unresolved_frame_boxes: list[tuple[int, list[BoundingBox]]], dummy_tracking: Tracking, type: str
+    session: Session,
+    framewise_unresolved_boxes: list[tuple[int, list[BoundingBox]]],
+    dummy_tracking: Tracking,
+    type: str,
 ) -> None:
-    for frame_nr, unresolved_boxes in unresolved_frame_boxes:
-        for unresolved_box in unresolved_boxes:
-            session.add(
-                TrackingFrameFeature(
-                    tracking=dummy_tracking,
-                    frame_nr=frame_nr,
-                    bbox_x_center=unresolved_box.x_center_n,
-                    bbox_y_center=unresolved_box.y_center_n,
-                    bbox_width=unresolved_box.width_n,
-                    bbox_height=unresolved_box.height_n,
-                    confidence=unresolved_box.confidence,
-                    type=type,
-                )
-            )
+    session.add_all(
+        TrackingFrameFeature(
+            tracking=dummy_tracking,
+            frame_nr=frame_nr,
+            bbox_x_center=box.x_center_n,
+            bbox_y_center=box.y_center_n,
+            bbox_width=box.width_n,
+            bbox_height=box.height_n,
+            confidence=box.confidence,
+            type=type,
+        )
+        for frame_nr, boxes in framewise_unresolved_boxes
+        for box in boxes
+    )
 
 
 def store_correlated_boxes(
@@ -74,20 +74,19 @@ def store_correlated_boxes(
     type: str,
     id_to_tracking: dict[int, Tracking],
 ) -> None:
-    for correlated_box in correlated_boxes:
-        bbox = correlated_box.bbox
-        session.add(
-            TrackingFrameFeature(
-                tracking=id_to_tracking[correlated_box.association],  # NOTE needed for data model validation
-                frame_nr=frame_nr,
-                bbox_x_center=bbox.x_center_n,
-                bbox_y_center=bbox.y_center_n,
-                bbox_width=bbox.width_n,
-                bbox_height=bbox.height_n,
-                confidence=bbox.confidence,
-                type=type,
-            )
+    session.add_all(
+        TrackingFrameFeature(
+            tracking=id_to_tracking[box.association],
+            frame_nr=frame_nr,
+            bbox_x_center=box.bbox.x_center_n,
+            bbox_y_center=box.bbox.y_center_n,
+            bbox_width=box.bbox.width_n,
+            bbox_height=box.bbox.height_n,
+            confidence=box.bbox.confidence,
+            type=type,
         )
+        for box in correlated_boxes
+    )
 
 
 def predict_correlate_store(
@@ -101,35 +100,31 @@ def predict_correlate_store(
 ) -> None:
     with session_cls() as session:
         video_tracking = load_video_tracking(session, video)
-
         assert video_tracking.frame_step == yolo_kwargs.get(
             "vid_stride", 1
         ), "vid_stride must match the frame_step of the body tracking"
 
-        id_to_tracking = get_id_to_tracking(video_tracking.trackings)
-        unresolved_frame_boxes: list[tuple[int, list[BoundingBox]]] = []
-        tracked_frames = load_tracked_frames(session, video_tracking, filter_by_type="body")
-        tracked_frames = tracked_frames[1:]  # NOTE(memben): https://github.com/ultralytics/ultralytics/issues/8976
+        id_to_tracking = {tracking.tracking_id: tracking for tracking in video_tracking.trackings}
+        framewise_unresolved_boxes: list[tuple[int, list[BoundingBox]]] = []
+
+        tracked_frames = load_tracked_frames(session, video_tracking, filter_by_type="body")[
+            1:
+        ]  # NOTE(memben): YOLOv8 skips the 0th frame https://github.com/ultralytics/ultralytics/issues/8976
+
         prediction: results.Results
         for prediction, tracked_frame in zip_longest(
             yolo_model.predict(video, stream=True, **yolo_kwargs), tracked_frames
         ):
             assert prediction is not None
             assert tracked_frame is not None
-
-            detected_bboxes = process_detections(prediction, video_tracking)
-            tracked_boxes = [
-                AssociatedBoundingBox(feature.tracking_id, BoundingBox.from_tracking_frame_feature(feature))
-                for feature in tracked_frame.frame_features
-            ]
-            correlated_boxes, uncorrelated_boxes = correlate(tracked_boxes, detected_bboxes, threshold=0.1)
-            unresolved_frame_boxes.append((tracked_frame.frame_nr, uncorrelated_boxes))
-
+            feature_bboxes = process_detections(prediction, video_tracking)
+            body_bboxes = convert_to_associated_bbox(tracked_frame)
+            correlated_boxes, uncorrelated_boxes = correlate(body_bboxes, feature_bboxes, threshold=0.1)
+            framewise_unresolved_boxes.append((tracked_frame.frame_nr, uncorrelated_boxes))
             store_correlated_boxes(session, correlated_boxes, tracked_frame.frame_nr, type, id_to_tracking)
 
-        log.info(
-            f"Unresolved boxes: {sum(len(boxes) for _, boxes in unresolved_frame_boxes)} in {sum(1 for _, boxes in unresolved_frame_boxes if boxes)} frames in video {video.name}, type {type}"
-        )
+        total_unresolved_boxes = sum(len(boxes) for _, boxes in framewise_unresolved_boxes)
+        log.info(f"{total_unresolved_boxes} unresolved boxes of type {type} in video {video.name} ")
         session.commit()
 
         if DANGER_activate_visual_debugging:
@@ -139,5 +134,5 @@ def predict_correlate_store(
                 tracking_id=(-1) * video_tracking.video_id,
             )
             session.add(dummy_tracking)
-            add_unresolved_boxes_for_debugging(session, unresolved_frame_boxes, dummy_tracking, type)
+            add_unresolved_boxes_for_debugging(session, framewise_unresolved_boxes, dummy_tracking, type)
             session.commit()
