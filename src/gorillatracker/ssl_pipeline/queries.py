7f0cf9593b272140287828939e726a3e3433a5aa
memben <ben.meyermeisel@gmail.com>
2024-03-17 01:19:01 +0000

    condense queries

--- src/gorillatracker/ssl_pipeline/queries.py
+++ src/gorillatracker/ssl_pipeline/queries.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Sequence
 
 from sqlalchemy import Select, select
 from sqlalchemy.orm import Session
@@ -7,20 +6,15 @@ from sqlalchemy.orm import Session
 from gorillatracker.ssl_pipeline.models import Tracking, TrackingFrameFeature, Video
 
 
-# TODO (memben): figure out how to type this
-def video_tracking_frame_features_query(video_id: int) -> Select:  # type: ignore
+def video_tracking_frame_features_query(video: Path) -> Select[tuple[TrackingFrameFeature]]:
     return (
         select(TrackingFrameFeature)
         .select_from(Video)
-        .where(Video.video_id == video_id)
+        .where(Video.filename == str(video.name))
         .join(Tracking)
         .join(TrackingFrameFeature)
     )
 
 
-def load_video_by_filename(session: Session, video_path: Path) -> Video:
-    return session.execute(select(Video).where(Video.filename == str(video_path.name))).scalar_one()
-
-
-def load_video_tracking_frame_features(session: Session, video_id: int) -> Sequence[TrackingFrameFeature]:
-    return session.execute(video_tracking_frame_features_query(video_id)).scalars().all()
+def load_video_by_filename(session: Session, video: Path) -> Video:
+    return session.execute(select(Video).where(Video.filename == str(video.name))).scalar_one()
