bafa78225660754d31decef6c9f6a7f418d89862
Joscha Schroff <joscha.schroff@gmail.com>
2024-04-09 14:47:06 +0000

    add read_timestamp to helpers.py

--- src/gorillatracker/ssl_pipeline/helpers.py
+++ src/gorillatracker/ssl_pipeline/helpers.py
@@ -8,6 +8,9 @@ from itertools import groupby
 from pathlib import Path
 from typing import Generator, Sequence
 
+from datetime import time
+import easyocr
+
 import cv2
 from shapely.geometry import Polygon
 
@@ -145,3 +148,49 @@ def groupby_frame(
 def remove_processed_videos(video_paths: list[Path], processed_videos: list[Video]) -> list[Path]:
     processed_video_paths = [Path(v.path) for v in processed_videos]
     return [v for v in video_paths if v not in processed_video_paths]
+
+
+def read_timestamp(video_path: Path, left: float = 0.61, top: float = 0.9, right: float = 0.75, bottom: float = 1.0) -> time:
+    """
+    Extracts the time stamp from the video file.
+    Args:
+        video_path (Path): path to the video file
+        left (float): left boundary of the crop area in %
+        top (float): top boundary of the crop area in %
+        right (float): right boundary of the crop area in %
+        bottom (float): bottom boundary of the crop area in %
+    Returns:
+        time: time stamp as time object
+    """
+    # open the video file
+    assert video_path.exists(), f"Video file {video_path} does not exist"
+    cap = cv2.VideoCapture(str(video_path))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    x_left, y_top, x_right, y_bottom = int(left * width), int(top * height), int(right * width), int(bottom * height)
+    crop_area = (x_left, y_top, x_right, y_bottom)
+    # read and crop frame
+    frame = cap.read()[1]
+    cropped_frame = frame[crop_area[1] : crop_area[3], crop_area[0] : crop_area[2]]
+    cropped_frame_rgb = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
+    reader = easyocr.Reader(["en"], gpu=False, verbose=False)
+    # extract timestamp
+    extracted_time_stamp_raw = reader.readtext(cropped_frame_rgb)
+    time_stamp = "".join([text[1] for text in extracted_time_stamp_raw])
+    # clean up timestamp
+    time_stamp = time_stamp.replace(" ", "")
+    time_stamp = time_stamp.replace(":", "")
+    time_stamp = time_stamp.replace("O", "0")
+    time_stamp = time_stamp.replace("o", "0")
+    cap.release()
+    try:
+        h = int(time_stamp[:2])
+        m = int(time_stamp[2:4])
+        am = True if time_stamp[4:6].lower() == "am" else False
+    except ValueError:
+        raise ValueError(f"Could not extract time stamp from {video_path}")
+    if not am and h < 12:
+        h += 12
+    if am and h == 12:
+        h = 0
+    return time(h, m)
