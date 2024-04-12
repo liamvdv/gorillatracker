cdac8f2e45323ee5e6c50f23c2a1b2a0b176ce18
Emirhan <emirhan.dogan404@gmail.com>
2024-04-12 15:02:49 +0000

    fixed typing and refactored notebook

--- src/gorillatracker/scripts/visualize_embeddings.py
+++ src/gorillatracker/scripts/visualize_embeddings.py
@@ -1,13 +1,13 @@
 import base64
 from io import BytesIO
-from typing import Any
+import numpy.typing as npt
 
 import colorcet as cc
 import numpy as np
 import pandas as pd
 import umap.umap_ as umap
 from bokeh.models import ColumnDataSource, HoverTool
-from bokeh.plotting import figure, output_file, save
+from bokeh.plotting import figure
 from sklearn.decomposition import PCA
 from sklearn.manifold import MDS, TSNE, Isomap, LocallyLinearEmbedding, SpectralEmbedding
 
@@ -24,7 +24,7 @@ class EmbeddingProjector:
             "umap": umap.UMAP(),
         }
 
-    def reduce_dimensions(self, embeddings: "pd.Series[Any]", method: str = "tsne") -> "np.ndarray[Any, Any]":
+    def reduce_dimensions(self, embeddings: npt.NDArray[np.float_], method: str = "tsne") -> npt.NDArray[np.float_]:
         assert len(embeddings) > 2
         algorithm = TSNE(n_components=2, perplexity=1)
         if len(embeddings) > 30:
@@ -33,13 +33,23 @@ class EmbeddingProjector:
 
     def plot_clusters(
         self,
-        low_dim_embeddings: "np.ndarray[Any, Any]",
-        labels: "pd.Series[Any]",
-        og_labels: "pd.Series[Any]",
-        images: "pd.Series[Any]",
+        low_dim_embeddings: npt.NDArray[np.float_],
+        labels: pd.Series,
+        og_labels: pd.Series,
+        images: list[str],  # base64.b64encode(buffer.getvalue()).decode("utf-8") --> buffer is a JPEG image inside of a BytesIO object
         title: str = "Embedding Projector",
         figsize: tuple[int, int] = (12, 10),
-    ) -> None:
+    ) -> figure:
+        """
+        after calling this use:
+        
+        if in notebook:
+        show(fig)
+        
+        if in script:
+        output_file(filename="embedding.html")
+        save(fig)
+        """
         color_names = cc.glasbey
         color_lst = [color_names[label * 2] for label in labels]
         data = {
@@ -64,8 +74,7 @@ class EmbeddingProjector:
         hover = HoverTool(tooltips='<img src="data:image/jpeg;base64,@image" width="128" height="128">')
         fig.add_tools(hover)
 
-        output_file(filename="embedding.html")
-        save(fig)
+        return fig
 
 
 def visualize_embeddings(
