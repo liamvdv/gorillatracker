bb42e16827c35f7cf25b3d885cc9240e40a03e29
Kajo Kratzenstein <kajo.kratzenstein@student.hpi.de>
2024-05-16 11:04:47 +0000

    up

--- src/gorillatracker/quantization/export_model.py
+++ src/gorillatracker/quantization/export_model.py
@@ -4,5 +4,5 @@ from typing import Tuple
 from gorillatracker.model import BaseModule
 
 
-def convert_model_to_onnx(model: BaseModule, input_shape: Tuple[int, int, int, int], output_path: str):
+def convert_model_to_onnx(model: BaseModule, input_shape: Tuple[int, int, int, int], output_path: str) -> None:
     torch.onnx.export(model, torch.randn(input_shape), output_path, opset_version=17)
