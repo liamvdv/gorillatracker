5fb141ca7c01b1fe29b201cfdb707d63d16a86e8
rob2u <robert.weeke@student.hpi.uni-potsdam.de>
2024-05-16 11:05:32 +0000

    fix formatting

--- src/gorillatracker/model.py
+++ src/gorillatracker/model.py
@@ -25,7 +25,7 @@ from torchvision.models import (
 from transformers import ResNetModel
 
 import gorillatracker.type_helper as gtypes
-from gorillatracker.losses.arcface_loss import ArcFaceLoss, VariationalPrototypeLearning
+from gorillatracker.losses.arcface_loss import VariationalPrototypeLearning
 from gorillatracker.losses.triplet_loss import L2SPRegularization_Wrapper, get_loss
 from gorillatracker.model_miewid import GeM, load_miewid_model  # type: ignore
 from gorillatracker.utils.labelencoder import LinearSequenceEncoder
