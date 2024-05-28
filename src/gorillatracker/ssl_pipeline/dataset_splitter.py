15284ab03daa638ca228f2137ff712b3ebeb93e1
Joscha Schroff <joscha.schroff@gmail.com>
2024-05-28 13:46:03 +0000

    time range for ssl split and add time splitter

--- src/gorillatracker/ssl_pipeline/dataset_splitter.py
+++ src/gorillatracker/ssl_pipeline/dataset_splitter.py
@@ -1,8 +1,9 @@
-import json
+import datetime as dt
 import os
 import pickle
+import sys
 from dataclasses import dataclass
-from typing import Any, Literal, Union
+from typing import Literal, Union
 
 from simple_parsing import field
 from sqlalchemy import Select, create_engine
@@ -17,24 +18,21 @@ class SplitArgs:
     db_uri: str
     version: str
     name: str = field(default="SSL-Video-Split")
-    split_by: Literal["percentage", "camera", "custom"] = field(default="percentage")
+    split_by: Literal["percentage", "camera", "time", "custom"] = field(default="percentage")
     save_path: str = field(default=".")
-    save_pickle: bool = field(default=False)
-    save_json: bool = field(default=False)
 
     train_split: int = field(default=80)
     val_split: int = field(default=10)
     test_split: int = field(default=10)
 
-    # include None to include videos without timestamp
-    train_years: list[Union[int, None]] = field(default_factory=lambda: list(range(2010, 2030)) + [None])
-    val_years: list[Union[int, None]] = field(default_factory=lambda: list(range(2010, 2030)) + [None])
-    test_years: list[Union[int, None]] = field(default_factory=lambda: list(range(2010, 2030)) + [None])
+    train_starttime: dt.datetime = field(default=dt.datetime(2010, 1, 1))
+    train_endtime: dt.datetime = field(default=dt.datetime(2030, 1, 1))
 
-    # include None to include videos without timestamp
-    train_months: list[Union[int, None]] = field(default_factory=lambda: list(range(1, 13)) + [None])
-    val_months: list[Union[int, None]] = field(default_factory=lambda: list(range(1, 13)) + [None])
-    test_months: list[Union[int, None]] = field(default_factory=lambda: list(range(1, 13)) + [None])
+    val_starttime: dt.datetime = field(default=dt.datetime(2010, 1, 1))
+    val_endtime: dt.datetime = field(default=dt.datetime(2030, 1, 1))
+
+    test_starttime: dt.datetime = field(default=dt.datetime(2010, 1, 1))
+    test_endtime: dt.datetime = field(default=dt.datetime(2030, 1, 1))
 
     # include None to include videos without timestamp
     hours: list[Union[int, None]] = field(default_factory=lambda: list(range(0, 24)) + [None])
@@ -49,17 +47,6 @@ class SplitArgs:
     _val_video_ids: list[int] = field(init=False, default=[])
     _test_video_ids: list[int] = field(init=False, default=[])
 
-    def __post_init__(self) -> None:
-        if self.split_by in ["percentage", "camera"]:
-            self.name = f"{self.name}_{self.version}_{self.split_by}-{self.train_split}-{self.val_split}-{self.test_split}_split"
-        else:
-            self.name = f"{self.name}_{self.version}_{self.split_by}_split"
-        self.create_split()
-        if self.save_pickle:
-            self.save_to_pickle()
-        if self.save_json:
-            self.save_to_json()
-
     def train_video_ids(self) -> list[int]:
         return self._train_video_ids
 
@@ -70,8 +57,15 @@ class SplitArgs:
         return self._test_video_ids
 
     def create_split(self) -> None:
+        if self.split_by in ["percentage", "camera", "time"]:
+            self.name = f"{self.name}_{self.version}_{self.split_by}-{self.train_split}-{self.val_split}-{self.test_split}_split"
+        else:
+            self.name = f"{self.name}_{self.version}_{self.split_by}_split"
+
         if self.split_by == "percentage":
             self.split_by_percentage()
+        elif self.split_by == "time":
+            self.split_by_time()
         elif self.split_by == "camera":
             self.split_by_cameras()
         elif self.split_by == "custom":
@@ -79,47 +73,55 @@ class SplitArgs:
         else:
             raise ValueError("Invalid split_by argument")
 
-    def build_train_query(self) -> Select[tuple[Video]]:
+    def build_query(
+        self, range: tuple[dt.datetime, dt.datetime], max_videos: int, allow_none_date: bool = False
+    ) -> Select[tuple[Video]]:
         query = vq.get_video_query(self.version)
-        query = vq.year_filter(query, self.train_years)
-        query = vq.month_filter(query, self.train_months)
+        query = vq.date_filter(query, range, allow_none_date)
         query = vq.hour_filter(query, self.hours)
         query = vq.video_length_filter(query, self.video_length[0], self.video_length[1])
-        query = vq.video_count_filter(query, self.max_train_videos)
+        query = vq.video_count_filter(query, max_videos)
+        return query
+
+    def build_train_query(self) -> Select[tuple[Video]]:
+        query = self.build_query((self.train_starttime, self.train_endtime), self.max_train_videos)
         return query
 
     def build_val_query(self) -> Select[tuple[Video]]:
-        query = vq.get_video_query(self.version)
-        query = vq.year_filter(query, self.val_years)
-        query = vq.month_filter(query, self.val_months)
-        query = vq.hour_filter(query, self.hours)
-        query = vq.video_length_filter(query, self.video_length[0], self.video_length[1])
-        query = vq.video_count_filter(query, self.max_val_videos)
+        query = self.build_query((self.val_starttime, self.val_endtime), self.max_val_videos)
         return query
 
     def build_test_query(self) -> Select[tuple[Video]]:
-        query = vq.get_video_query(self.version)
-        query = vq.year_filter(query, self.test_years)
-        query = vq.month_filter(query, self.test_months)
-        query = vq.hour_filter(query, self.hours)
-        query = vq.video_length_filter(query, self.video_length[0], self.video_length[1])
-        query = vq.video_count_filter(query, self.max_test_videos)
+        query = self.build_query((self.test_starttime, self.test_endtime), self.max_test_videos)
         return query
+
+    def split_by_time(self) -> None:
+        """Split the videos based on the time range."""
+        engine = create_engine(self.db_uri)
+        session = sessionmaker(bind=engine)
+        query = self.build_query((dt.datetime.min, dt.datetime.max), sys.maxsize)
+        query = vq.order_by_time(query)
+        videos = vq.get_videos_from_query(query, session())
+        train_end = int(len(videos) * self.train_split / 100)
+        val_end = train_end + int(len(videos) * self.val_split / 100)
+        self._train_video_ids = (videos[:train_end])[: self.max_train_videos]
+        self._val_video_ids = (videos[train_end:val_end])[: self.max_val_videos]
+        self._test_video_ids = (videos[val_end:])[: self.max_test_videos]
 
     def split_by_percentage(self) -> None:
         """Split the videos based on the percentage split."""
         assert (
             self.train_split + self.val_split + self.test_split == 100
         ), "The sum of the split percentages must be 100"
-        query = self.build_train_query()
         engine = create_engine(self.db_uri)
         session = sessionmaker(bind=engine)
+        query = self.build_query((dt.datetime.min, dt.datetime.max), sys.maxsize, True)
         videos = vq.get_videos_from_query(query, session())
         train_end = int(len(videos) * self.train_split / 100)
         val_end = train_end + int(len(videos) * self.val_split / 100)
-        self._train_video_ids = videos[:train_end]
-        self._val_video_ids = videos[train_end:val_end]
-        self._test_video_ids = videos[val_end:]
+        self._train_video_ids = (videos[:train_end])[: self.max_train_videos]
+        self._val_video_ids = (videos[train_end:val_end])[: self.max_val_videos]
+        self._test_video_ids = (videos[val_end:])[: self.max_test_videos]
 
     def split_by_cameras(self) -> None:
         """Split the videos based on camera ids."""
@@ -134,13 +136,13 @@ class SplitArgs:
         test_cameras = cameras[len(train_cameras) + len(val_cameras) :]
         self._train_video_ids = vq.get_videos_from_query(
             vq.camera_id_filter(self.build_train_query(), train_cameras), session()
-        )
+        )[: self.max_train_videos]
         self._val_video_ids = vq.get_videos_from_query(
             vq.camera_id_filter(self.build_val_query(), val_cameras), session()
-        )
+        )[: self.max_val_videos]
         self._test_video_ids = vq.get_videos_from_query(
             vq.camera_id_filter(self.build_test_query(), test_cameras), session()
-        )
+        )[: self.max_test_videos]
 
     def split_custom(self) -> None:
         """Split the videos based on custom criteria."""
@@ -155,48 +157,16 @@ class SplitArgs:
         self._test_video_ids = vq.get_videos_from_query(test_query, session())
         assert len(self._train_video_ids) != 0 and len(self._val_video_ids) != 0, "train and val cannot be empty"
 
-    def config_to_json(self) -> dict[str, Any]:
-        """Return the configuration as a dictionary."""
-        return {
-            "name": self.name,
-            "split_by": self.split_by,
-            "train_split": self.train_split,
-            "val_split": self.val_split,
-            "test_split": self.test_split,
-            "train_years": self.train_years,
-            "val_years": self.val_years,
-            "test_years": self.test_years,
-            "train_months": self.train_months,
-            "val_months": self.val_months,
-            "test_months": self.test_months,
-            "hours": self.hours,
-            "video_length": self.video_length,
-            "max_train_videos": self.max_train_videos,
-            "max_val_videos": self.max_val_videos,
-            "max_test_videos": self.max_test_videos,
-        }
-
-    def save_to_json(self) -> None:
-        """Write the split to a json file."""
-        config = self.config_to_json()
-        file_path = os.path.join(self.save_path, f"{self.name}.json")
-        with open(file_path, "w") as f:
-            json.dump(
-                {
-                    "config": config,
-                    "train": self._train_video_ids,
-                    "val": self._val_video_ids,
-                    "test": self._test_video_ids,
-                },
-                f,
-                indent=4,
-            )
-
     def save_to_pickle(self) -> None:
         """Save the class instance to a pickle file."""
         file_path = os.path.join(self.save_path, f"{self.name}.pkl")
         with open(file_path, "wb") as file:
             pickle.dump(self, file)
+
+    def load_pickle(self, path: str) -> None:
+        """Load the class instance from a pickle file."""
+        with open(path, "rb") as file:
+            return pickle.load(file)
 
 
 if __name__ == "__main__":
@@ -205,17 +175,15 @@ if __name__ == "__main__":
         version="2024-04-18",
         save_path="/workspaces/gorillatracker/data/splits/SSL/",
         split_by="percentage",
-        save_pickle=True,
-        save_json=False,
         train_split=80,
         val_split=10,
         test_split=10,
-        train_years=list(range(2010, 2030)),  # only videos from certain years
-        val_years=list(range(2010, 2030)),  # only videos from certain years
-        test_years=list(range(2010, 2030)),  # only videos from certain years
-        train_months=list(range(0, 13)),  # only videos from certain months of the year
-        val_months=list(range(0, 13)),  # only videos from certain months of the year
-        test_months=list(range(0, 13)),  # only videos from certain months of the year
+        train_starttime=dt.datetime(2010, 1, 1),
+        train_endtime=dt.datetime(2030, 1, 1),
+        val_starttime=dt.datetime(2010, 1, 1),
+        val_endtime=dt.datetime(2030, 1, 1),
+        test_starttime=dt.datetime(2010, 1, 1),
+        test_endtime=dt.datetime(2030, 1, 1),
         hours=list(range(0, 24)),  # only videos from certain hours of the day
         video_length=(0, 1000000),  # min, max video length in seconds
         max_train_videos=1000000,  # max videos in train bucket
