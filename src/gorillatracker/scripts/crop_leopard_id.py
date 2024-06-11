fc0f7b784c853228f1bd9d2913576207a4fa6b4d
V1nce1 <vincent.eberwein@gmail.com>
2024-06-11 14:54:48 +0000

    format, lint and mypy

--- src/gorillatracker/scripts/crop_leopard_id.py
+++ src/gorillatracker/scripts/crop_leopard_id.py
@@ -1,21 +1,16 @@
 import json
 import os
+from typing import Optional
 
 import cv2
 from tqdm import tqdm
 
 
-def load_json(file_path: str) -> dict:
-    with open(file_path, "r") as f:
-        return json.load(f)
-
-def extract_image_file_names(images: list) -> dict:
+def extract_image_file_names(images: list) -> dict[str, str]:  # type: ignore
     return {img["id"]: img["file_name"] for img in images}
 
-def is_valid_leopard_name(name: str) -> bool:
-    return name.count("-") == 4
 
-def load_image(image_path: str) -> cv2.Mat:
+def load_image(image_path: str) -> Optional[cv2.Mat]:
     if not os.path.exists(image_path):
         print(f"Image file does not exist: {image_path}")
         return None
@@ -25,19 +20,23 @@ def load_image(image_path: str) -> cv2.Mat:
         return None
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-def crop_image(image: cv2.Mat, bbox: list) -> cv2.Mat:
+
+def crop_image(image: cv2.Mat, bbox: list) -> cv2.Mat:  # type: ignore
     x, y, width, height = bbox
     return image[int(y) : int(y + height), int(x) : int(x + width)]
 
-def filter_duplicate_annotations(annotations: list) -> list:
+
+def filter_duplicate_annotations(annotations: list) -> list:  # type: ignore
     image_ids = [ann["image_id"] for ann in annotations]
     duplicate_image_ids = [image_id for image_id in image_ids if image_ids.count(image_id) > 1]
 
     return [ann for ann in annotations if ann["image_id"] not in duplicate_image_ids]
 
+
 def crop_images_from_coco(annotation_path: str, image_dir: str, output_dir: str) -> None:
     os.makedirs(output_dir, exist_ok=True)
-    annotation_file = load_json(annotation_path)
+    with open(annotation_path, "r") as f:
+        annotation_file = json.load(f)
     annotations = annotation_file.get("annotations", [])
     # There are images with multiple annotations and different names which need to be filtered out
     annotations = filter_duplicate_annotations(annotations)
@@ -46,20 +45,14 @@ def crop_images_from_coco(annotation_path: str, image_dir: str, output_dir: str)
     for ann in tqdm(annotations, desc="Cropping images", total=len(annotations), unit="image"):
         image_id = ann["image_id"]
         leopard_name = ann["name"]
-        if not is_valid_leopard_name(leopard_name):
-            print(f"Invalid leopard name: {leopard_name}")
-            continue
-
         image_file = image_file_names.get(image_id)
         if image_file is None:
             print(f"Image file not found for image_id: {image_id}")
             continue
-
         image_path = os.path.join(image_dir, image_file)
         image = load_image(image_path)
         if image is None:
             continue
-
         cropped_image = crop_image(image, ann["bbox"])
         output_path = os.path.join(output_dir, f"{leopard_name}_{image_id}.png")
         cv2.imwrite(output_path, cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR))
@@ -69,5 +62,5 @@ if __name__ == "__main__":
     data_dir = "/workspaces/gorillatracker/datasets/vast/external-datasets/LeopardID2022/leopard.coco"
     annotation_file = os.path.join(data_dir, "annotations/instances_train2022.json")
     image_dir = os.path.join(data_dir, "images/train2022")
-    output_dir = "/workspaces/gorillatracker/datasets/vast/external-datasets/LeopardID2022/cropped_images/train"
+    output_dir = "compressed/test"
     crop_images_from_coco(annotation_file, image_dir, output_dir)
