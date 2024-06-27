c51464916d8001c075d6eef07f22db1ddd968b1b
rob2u <robert.weeke@student.hpi.uni-potsdam.de>
2024-06-27 10:58:31 +0000

    rename crossencounter to crossvideo

--- src/gorillatracker/model.py
+++ src/gorillatracker/model.py
@@ -570,10 +570,10 @@ class BaseModule(L.LightningModule):
         )
         metrics |= (
             {
-                "knn_crossencounter": partial(knn, k=1, use_crossvideo_positives=True),
-                "knn5_crossencounter": partial(knn, k=5, use_crossvideo_positives=True),
-                "knn_crossencounter_macro": partial(knn, k=1, use_crossvideo_positives=True, average="macro"),
-                "knn5_crossencounter_macro": partial(knn, k=5, use_crossvideo_positives=True, average="macro"),
+                "knn_crossvideo": partial(knn, k=1, use_crossvideo_positives=True),
+                "knn5_crossvideo": partial(knn, k=5, use_crossvideo_positives=True),
+                "knn_crossvideo_macro": partial(knn, k=1, use_crossvideo_positives=True, average="macro"),
+                "knn5_crossvideo_macro": partial(knn, k=5, use_crossvideo_positives=True, average="macro"),
             }
             if "cxl" in dataloader_id.lower() or "bristol" in dataloader_id.lower()
             else {}
