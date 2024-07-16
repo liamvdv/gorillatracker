d1f4fe4f7eb4235892b077a44ac93595bbeadc0e
V1nce1 <vincent.eberwein@gmail.com>
2024-07-16 08:23:45 +0000

    BYOL WIP and transforms

--- src/gorillatracker/model/wrappers_ssl.py
+++ src/gorillatracker/model/wrappers_ssl.py
@@ -4,13 +4,15 @@ from typing import Callable
 import timm
 import torch
 import torchvision.transforms.v2 as transforms_v2
-from lightly.models.modules.heads import MoCoProjectionHead, SimCLRProjectionHead
+from lightly.models.modules.heads import MoCoProjectionHead, SimCLRProjectionHead, BYOLProjectionHead
 from lightly.models.utils import deactivate_requires_grad, update_momentum
+from lightly.loss import NegativeCosineSimilarity
 from torchvision import transforms
 
 import gorillatracker.type_helper as gtypes
 from gorillatracker.data.utils import flatten_batch
 from gorillatracker.model.base_module import BaseModule
+from gorillatracker.transform_utils import PlanckianJitter
 
 
 class SimCLRWrapper(BaseModule):
@@ -33,8 +35,15 @@ class SimCLRWrapper(BaseModule):
     def get_training_transforms(cls) -> Callable[[torch.Tensor], torch.Tensor]:
         return transforms.Compose(
             [
-                transforms.RandomErasing(p=0.5, scale=(0.02, 0.13)),
+                transforms_v2.ToPILImage(),
+                PlanckianJitter(),
+                transforms_v2.ToTensor(),
+                # transforms_v2.RandomPhotometricDistort(p=0.5),
                 transforms_v2.RandomHorizontalFlip(p=0.5),
+                transforms_v2.RandomErasing(p=0.5, value=0, scale=(0.02, 0.13)),
+                transforms_v2.RandomRotation(60, fill=0),
+                transforms_v2.RandomResizedCrop(224, scale=(0.75, 1.0)),
+                
             ]
         )
 
@@ -69,7 +78,7 @@ class MoCoWrapper(BaseModule):
         if self.use_inbatch_mixup:
             flat_images, flat_labels_onehot = self.perform_mixup(flat_images, flat_labels)
         anchor_embeddings = self.model(images[0])
-        positive_embeddings = self.model_momentum(images[1])
+        positive_embeddings = self.model_momentum(images[1]).detach()
         embeddings = torch.cat([anchor_embeddings, positive_embeddings])
 
         assert not torch.isnan(embeddings).any(), f"Embeddings are NaN: {embeddings}"
@@ -86,7 +95,80 @@ class MoCoWrapper(BaseModule):
     def get_training_transforms(cls) -> Callable[[torch.Tensor], torch.Tensor]:
         return transforms.Compose(
             [
-                transforms.RandomErasing(p=0.5, scale=(0.02, 0.13)),
+                transforms_v2.ToPILImage(),
+                PlanckianJitter(),
+                transforms_v2.ToTensor(),
+                # transforms_v2.RandomPhotometricDistort(p=0.5),
                 transforms_v2.RandomHorizontalFlip(p=0.5),
+                transforms_v2.RandomErasing(p=0.5, value=0, scale=(0.02, 0.13)),
+                transforms_v2.RandomRotation(60, fill=0),
+                transforms_v2.RandomResizedCrop(224, scale=(0.75, 1.0)),
+                
+            ]
+        )
+        
+class BYOLWrapper(BaseModule):
+    def __init__(  # type: ignore
+        self,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.model = timm.create_model(
+            "vit_large_patch14_dinov2.lvd142m", pretrained=not self.from_scratch, img_size=224
+        )
+        self.model.reset_classifier(self.embedding_size)
+        in_features = self.model.head.in_features
+        self.model.head = BYOLProjectionHead(in_features, 1024, self.embedding_size)
+        self.prediction_head = BYOLProjectionHead(self.embedding_size, 1024, self.embedding_size)
+        model_cpy = copy.deepcopy(self.model)
+        model_cpy.head = torch.nn.Identity()
+        self.set_losses(model=model_cpy, **kwargs)
+        self.criterion = NegativeCosineSimilarity()
+
+        self.model_momentum = copy.deepcopy(self.model)
+        deactivate_requires_grad(self.model_momentum)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        y = self.model(x)
+        z = self.prediction_head(y)
+        return z
+
+    def forward_momentum(self, x: torch.Tensor) -> torch.Tensor:
+        y = self.model_momentum(x)
+        y = y.detach()
+        return y
+
+    def training_step(self, batch: gtypes.NletBatch, batch_idx: int) -> torch.Tensor:
+        _, images, _ = batch
+        _, flat_images, flat_labels = flatten_batch(batch)
+
+        update_momentum(self.model, self.model_momentum, 0.99)
+
+        # Calculate both embeddings and momentum embeddings for anchor and positive images
+        anchor_embeddings = self.model(images[0])
+        positive_embeddings = self.model(images[1])
+        
+        anchor_embeddings_momentum = self.forward_momentum(images[0])
+        positive_embeddings_momentum = self.forward_momentum(images[1])
+
+        loss = 0.5 * (self.criterion(anchor_embeddings, positive_embeddings_momentum) + self.criterion(anchor_embeddings_momentum, positive_embeddings))
+
+        log_str_prefix = f"fold-{self.kfold_k}/" if self.kfold_k is not None else ""
+        self.log(f"{log_str_prefix}train/loss", loss, on_step=True, prog_bar=True, sync_dist=True)
+        return loss
+    
+    @classmethod
+    def get_training_transforms(cls) -> Callable[[torch.Tensor], torch.Tensor]:
+        return transforms.Compose(
+            [
+                transforms_v2.ToPILImage(),
+                PlanckianJitter(),
+                transforms_v2.ToTensor(),
+                # transforms_v2.RandomPhotometricDistort(p=0.5),
+                transforms_v2.RandomHorizontalFlip(p=0.5),
+                transforms_v2.RandomErasing(p=0.5, value=0, scale=(0.02, 0.13)),
+                transforms_v2.RandomRotation(60, fill=0),
+                transforms_v2.RandomResizedCrop(224, scale=(0.75, 1.0)),
+                
             ]
         )
