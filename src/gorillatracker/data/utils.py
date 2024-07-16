1cc204000a07f6c22054dde1588c5371809a6dfa
Ben Meyer-Meisel <ben.meyermeisel@gmail.com>
2024-07-16 11:50:01 +0000

    add docstring

--- src/gorillatracker/data/utils.py
+++ src/gorillatracker/data/utils.py
@@ -11,6 +11,8 @@ def lazy_batch_size(batch: NletBatch) -> int:
 
 
 def flatten_batch(batch: NletBatch) -> FlatNletBatch:
+    """Returns one tuple for ids, images, labels. Each id/image/label has size N * batch size,
+    where N is from Nlet. E.g. for Quadlets, it would be `[anchors, positives, negatives, anchor_negative]`."""
     ids, images, labels = batch
     # transform ((a1, a2), (p1, p2), (n1, n2)) to (a1, a2, p1, p2, n1, n2)
     flat_ids = tuple(chain.from_iterable(ids))
