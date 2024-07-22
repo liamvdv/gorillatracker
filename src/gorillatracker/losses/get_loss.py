ad45ade0dfa7eaeeef25fe2d2d7c7f7027246e8d
rob2u <robert.weeke@student.hpi.uni-potsdam.de>
2024-07-22 11:18:44 +0000

    fix passing issue

--- src/gorillatracker/losses/get_loss.py
+++ src/gorillatracker/losses/get_loss.py
@@ -30,11 +30,12 @@ def get_loss(
         ), "class_distribution must be set for class weights"
 
     if loss_mode == "online/hard":
-        loss_module = TripletLossOnline(mode="hard", margin=kw_args["margin"])
+        loss_module = TripletLossOnline(mode="hard", margin=kw_args["margin"], dist_calc=kw_args["loss_dist_term"])
     elif loss_mode == "online/semi-hard":
         loss_module = TripletLossOnline(mode="semi-hard", margin=kw_args["margin"], dist_calc=kw_args["loss_dist_term"])
     elif loss_mode == "online/soft":
-        loss_module = TripletLossOnline(mode="soft", margin=kw_args["margin"])
+        print(f"using {kw_args['loss_dist_term']} distance term")
+        loss_module = TripletLossOnline(mode="soft", margin=kw_args["margin"], dist_calc=kw_args["loss_dist_term"])
     elif loss_mode == "offline":
         loss_module = TripletLossOffline(margin=kw_args["margin"])
     elif loss_mode == "offline/native":
